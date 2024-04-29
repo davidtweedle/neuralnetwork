614d499c0a319e09f71c9c59a0f35dc3ff906d0d
David Tweedle <david.aig.tweedle@gmail.com>
2024-04-29 11:10:33 -0400

    Error in log loss

--- genericdlmodel.py
+++ genericdlmodel.py
@@ -651,7 +651,7 @@ class ObjFunc:
         -log(y_i) such that y_hat_i == 1
 
         """
-        return -np.sum(np.log(y+self.eps, where=y_hat == 1))
+        return -np.sum(np.log(y + self.eps, where=y_hat > 0.5))
 
 
 class Updater():
