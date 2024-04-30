5d5aea007b6f357055933764e98f22ad0acdc627
David Tweedle <david.aig.tweedle@gmail.com>
2024-04-30 09:45:38 -0400

    Trying to find overflow error

--- genericdlmodel.py
+++ genericdlmodel.py
@@ -662,6 +662,8 @@ class ObjFunc:
         res = -np.sum(np.log(y, where=np.logical_and(y > self.eps,y_hat > 0.5)))
         if res < 0:
             print(y, y_hat)
+            print(res)
+            print(-np.sum(np.log(y, where=np.logical_and(y > self.eps, y_hat > 0.5))))
             raise Exception
         return res
 
